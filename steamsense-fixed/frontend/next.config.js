946e42fc06f654c5971086cdbb41e67113d8929c
cozi <101955124+C0z1@users.noreply.github.com>
2026-02-24 03:02:22 -0600

    Delete steamsense-fixed directory

--- steamsense-fixed/frontend/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  images: {
-    domains: ['cdn.cloudflare.steamstatic.com', 'shared.akamai.steamstatic.com'],
-  },
-}
-
-module.exports = nextConfig
